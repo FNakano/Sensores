c1b661e3bf24b677f9dcd1d4e46da3c3d7ab0d4c
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-05-24 17:50:51 -0300

    wifi

---
+++ ESP32/testeWifiPadraoFirebase32.ino
@@ -0,0 +1,245 @@
+#include <WiFi.h>        // Include the Wi-Fi library
+#include "FirebaseESP32.h"
+ 
+const char* ssid     = "Alexandre";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "lele8c35aebc";     // The password of the Wi-Fi network
+
+#define FIREBASE_HOST "redesensoresgit.firebaseio.com" //Do not include https:// in FIREBASE_HOST
+#define FIREBASE_AUTH "Big6OmN1pQLrHByhrVI99Ne5Mfaq3PvyLC0wnz7G"
+
+//Define Firebase Data object
+FirebaseData firebaseData;
+ 
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+ 
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(500);
+    Serial.print('.');
+  }
+ 
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+
+
+  /*Firebase configs*/
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  String path = "/ESP32_Test";
+  String jsonStr;
+
+  //Firebase.deleteNode(firebaseData, path);
+
+  Serial.println("------------------------------------");
+  Serial.println("Path exist test...");
+  if (Firebase.pathExist(firebaseData, path))
+  {
+    Serial.println("Path " + path + " exists");
+  }
+  else
+  {
+    Serial.println("Path " + path + " is not exist");
+  }
+  Serial.println("------------------------------------");
+  Serial.println();
+
+  Serial.println("------------------------------------");
+  Serial.println("Set integer test...");
+
+  for (uint8_t i = 0; i < 10; i++)
+  {
+
+    if (Firebase.setInt(firebaseData, path + "/Int/Data" + (i + 1), (i + 1) * 10))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.println("TYPE: " + firebaseData.dataType());
+      Serial.print("VALUE: ");
+      if (firebaseData.dataType() == "int")
+        Serial.println(firebaseData.intData());
+      else if (firebaseData.dataType() == "float")
+        Serial.println(firebaseData.floatData());
+      else if (firebaseData.dataType() == "boolean")
+        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+      else if (firebaseData.dataType() == "string")
+        Serial.println(firebaseData.stringData());
+      else if (firebaseData.dataType() == "json")
+        Serial.println(firebaseData.jsonData());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+  Serial.println("------------------------------------");
+  Serial.println("Get integer test...");
+
+  for (uint8_t i = 0; i < 10; i++)
+  {
+
+    if (Firebase.getInt(firebaseData, path + "/Int/Data" + (i + 1)))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.println("TYPE: " + firebaseData.dataType());
+      Serial.print("VALUE: ");
+      if (firebaseData.dataType() == "int")
+        Serial.println(firebaseData.intData());
+      else if (firebaseData.dataType() == "float")
+        Serial.println(firebaseData.floatData());
+      else if (firebaseData.dataType() == "boolean")
+        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+      else if (firebaseData.dataType() == "string")
+        Serial.println(firebaseData.stringData());
+      else if (firebaseData.dataType() == "json")
+        Serial.println(firebaseData.jsonData());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+  Serial.println("------------------------------------");
+  Serial.println("Push integer test...");
+
+  for (uint8_t i = 0; i < 5; i++)
+  {
+
+    if (Firebase.pushInt(firebaseData, path + "/Push/Int", (i + 1)))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.print("PUSH NAME: ");
+      Serial.println(firebaseData.pushName());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+  Serial.println("------------------------------------");
+  Serial.println("Push JSON test...");
+
+  for (uint8_t i = 5; i < 10; i++)
+  {
+
+    jsonStr = "{\"Data" + String(i + 1) + "\":" + String(i + 1) + "}";
+
+    if (Firebase.pushJSON(firebaseData, path + "/Push/Int", jsonStr))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.print("PUSH NAME: ");
+      Serial.println(firebaseData.pushName());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+  Serial.println("------------------------------------");
+  Serial.println("Update test...");
+
+  for (uint8_t i = 0; i < 5; i++)
+  {
+
+    jsonStr = "{\"Data" + String(i + 1) + "\":" + String(i + 5.5) + "}";
+
+    if (Firebase.updateNode(firebaseData, path + "/Int", jsonStr))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.println("TYPE: " + firebaseData.dataType());
+      Serial.print("VALUE: ");
+      if (firebaseData.dataType() == "int")
+        Serial.println(firebaseData.intData());
+      else if (firebaseData.dataType() == "float")
+        Serial.println(firebaseData.floatData());
+      else if (firebaseData.dataType() == "boolean")
+        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+      else if (firebaseData.dataType() == "string")
+        Serial.println(firebaseData.stringData());
+      else if (firebaseData.dataType() == "json")
+        Serial.println(firebaseData.jsonData());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  }
+
+  Serial.println("------------------------------------");
+  Serial.println("Get JSON test...");
+  if (Firebase.getJSON(firebaseData, path))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.print("VALUE: ");
+    if (firebaseData.dataType() == "int")
+      Serial.println(firebaseData.intData());
+    else if (firebaseData.dataType() == "float")
+      Serial.println(firebaseData.floatData());
+    else if (firebaseData.dataType() == "boolean")
+      Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+    else if (firebaseData.dataType() == "string")
+      Serial.println(firebaseData.stringData());
+    else if (firebaseData.dataType() == "json")
+      Serial.println(firebaseData.jsonData());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  //Quit Firebase and release all resources
+  Firebase.end(firebaseData);
+  
+}
+ 
+void loop() { 
+  
+}
